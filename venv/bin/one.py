e616b03db8a489e7df275c1c60adfcf1f6240d6c
fadallalaaisha <aishafadallala@gmail.com>
2021-05-25 18:00:09 +0300

    first commit

---
+++ venv/bin/one.py
@@ -0,0 +1,14 @@
+import calendar
+year=2021
+print(calendar.calendar(year))
+
+# is_leap_year=calendar.isleap(2020)
+# print(is_leap_year)
+
+import calendar
+year=2019
+print(calendar.calendar(year))
+
+import calendar
+year=2022
+print(calendar.calendar(year))
